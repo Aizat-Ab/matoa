8997572cbd5d38f78e232358dadb30ac93400442
Your Name <you@example.com>
2021-05-15 20:42:43 +0300

    add first components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/header";
+import './index.scss';
+
+
+function App() {
+  return (
+    <div className="App container">
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
