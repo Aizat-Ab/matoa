63897c4a0d0c9da55f3d1a20d6c88a3a25f42639
Your Name <you@example.com>
2021-08-25 18:31:30 +0300

    last

--- src/components/series/index.jsx
+++ src/components/series/index.jsx
@@ -9,7 +9,7 @@ const Series = () => {
     const [watch, setWhatches] = React.useState([]);
 
     React.useEffect(async()=>{
-        const responce = await axios.get('https://localhost:3008/watches');
+        const responce = await axios.get('https://matoa.herokuapp.com/api');
         setWhatches(responce.data);
       },[]); 
       
