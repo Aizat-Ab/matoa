cb9419640592221283e5bc4a64552d4bf94b65f0
Your Name <you@example.com>
2021-08-25 18:01:44 +0300

    ..

--- server.js
+++ server.js
@@ -13,26 +13,3 @@ server.use(router);
 server.listen(PORT, () => {
   console.log('Server is running');
 });
-
-
-// const express = require('express')
-// const app = express()
-// const port = process.env.PORT || 5000
-// const path = require('path')
-
-// app.use(express.static(path.join(__dirname, 'build')))
-
-// //all your api code goes here
-// app.get('/api', (req, res) => {
-//   res.json({
-//     message: 'This is the api endpoint'
-//   })
-// })
-
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'))
-// })
-
-// app.listen(port, _ => {
-//   console.log(`server started on port ${port}`)
-// })
